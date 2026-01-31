3c7ba36d9d9260f57248aeb586f9894ff8256e61
SeungKyun Park <skpark597@gmail.com>
2026-01-31 21:53:43 +0900

    Util: Implement isSameTree method

--- cpp/common/TreeNode.h
+++ cpp/common/TreeNode.h
@@ -1,19 +1,21 @@
 #include <vector>
+
 #include "limits.h"
 using namespace std;
 
 const int NONE = INT_MAX;
 class TreeNode {
-public:
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode();     
-	TreeNode(int x); 
-	TreeNode(int x, TreeNode *left, TreeNode *right);
+ public:
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode();
+  TreeNode(int x);
+  TreeNode(int x, TreeNode* left, TreeNode* right);
 };
 
 namespace util {
-	TreeNode *makeTree(vector<int> nums);
-	void printTree(TreeNode *root);
-}
+TreeNode* makeTree(vector<int> nums);
+bool isSameTree(TreeNode* root0, TreeNode root1);
+void printTree(TreeNode* root);
+}  // namespace util
