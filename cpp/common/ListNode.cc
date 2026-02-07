6c16d0ebaf1d0690b3fc82f24f13ce7f2dedd7f2
SeungKyun Park <skpark597@gmail.com>
2026-02-07 17:35:51 +0900

    Util: Implement isSameLinkedList method

--- cpp/common/ListNode.cc
+++ cpp/common/ListNode.cc
@@ -1,44 +1,52 @@
-#include <iostream>
 #include "ListNode.h"
+
+#include <iostream>
 
 ListNode::ListNode() : val(0), next(nullptr) {}
 ListNode::ListNode(int x) : val(x), next(nullptr) {}
-ListNode::ListNode(int x, ListNode *next) : val(x), next(next) {}
+ListNode::ListNode(int x, ListNode* next) : val(x), next(next) {}
 
-namespace util { 
-    ListNode *makeLinkedList(vector<int> nums) {
-        ListNode *head = nullptr;
-        ListNode *prev = nullptr;
+namespace util {
+ListNode* makeLinkedList(vector<int> nums) {
+  ListNode* head = nullptr;
+  ListNode* prev = nullptr;
 
-        if (nums.empty()) return nullptr;
+  if (nums.empty()) return nullptr;
 
-        head = new ListNode(nums[0]);
-        prev = head;
+  head = new ListNode(nums[0]);
+  prev = head;
 
-        for (int i = 1; i < nums.size(); ++i) {
-            ListNode *newNode = new ListNode(nums[i]);
-            prev->next = newNode;
-            prev = newNode;
-        }
+  for (int i = 1; i < nums.size(); ++i) {
+    ListNode* newNode = new ListNode(nums[i]);
+    prev->next = newNode;
+    prev = newNode;
+  }
 
-        return head;
-    }
-
-    void printList(ListNode *head) {
-        ListNode *iter = head;
-
-        if (head == nullptr) {
-            cout << "no element" << endl;
-            return;
-        }
-
-        cout << "[";
-        while (iter->next != nullptr) {
-            cout << iter->val << ", ";
-            iter = iter->next;
-        }
-
-        cout << iter->val << "]" << endl;
-    } 
+  return head;
 }
 
+void printList(ListNode* head) {
+  ListNode* iter = head;
+
+  if (head == nullptr) {
+    cout << "no element" << endl;
+    return;
+  }
+
+  cout << "[";
+  while (iter->next != nullptr) {
+    cout << iter->val << ", ";
+    iter = iter->next;
+  }
+
+  cout << iter->val << "]" << endl;
+}
+
+bool isSameLinkedList(ListNode* node0, ListNode* node1) {
+  if (!node0 && !node1) return true;
+  if (!node0 || !node1 || node0->val != node1->val) return false;
+
+  return isSameLinkedList(node0->next, node1->next);
+}
+
+}  // namespace util
--- cpp/common/ListNode.h
+++ cpp/common/ListNode.h
@@ -2,15 +2,16 @@
 using namespace std;
 
 class ListNode {
-public:
-	int val;
-    ListNode *next;
-    ListNode();
-    ListNode(int x);
-	ListNode(int x, ListNode *next);
+ public:
+  int val;
+  ListNode* next;
+  ListNode();
+  ListNode(int x);
+  ListNode(int x, ListNode* next);
 };
 
 namespace util {
-    ListNode *makeLinkedList(vector<int> nums);
-	void printList(ListNode *head);
-}
+ListNode* makeLinkedList(vector<int> nums);
+bool isSameLinkedList(ListNode* node0, ListNode* node1);
+void printList(ListNode* head);
+}  // namespace util
