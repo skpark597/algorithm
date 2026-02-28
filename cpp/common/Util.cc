184b3d7603200ee5ebb2a91c2955257763b9a7e7
SeungKyun Park <skpark597@gmail.com>
2026-02-28 17:09:19 +0900

    Fix: Unify the parameter types for the isEqualUnordered method

--- cpp/common/Util.cc
+++ cpp/common/Util.cc
@@ -6,8 +6,7 @@
 using namespace std;
 
 namespace util {
-bool isEqualIgnoringOrder(vector<vector<int>>& vec0,
-                          vector<vector<int>>& vec1) {
+bool isEqualUnordered(vector<vector<int>>& vec0, vector<vector<int>>& vec1) {
   if (vec0.size() != vec1.size()) return false;
 
   vector<vector<int>> new_vec0 = vec0;
--- cpp/common/Util.h
+++ cpp/common/Util.h
@@ -4,6 +4,6 @@
 using namespace std;
 
 namespace util {
-bool isEqualIgnoringOrder(vector<vector<int>> vec0, vector<vector<int>> vec1);
+bool isEqualUnordered(vector<vector<int>>& vec0, vector<vector<int>>& vec1);
 
 }
