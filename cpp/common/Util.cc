03dee236bdf696ed712cbd593fe0fbb6935c87db
SeungKyun Park <skpark597@gmail.com>
2026-02-28 17:05:25 +0900

    Util: Implement isEqualIgnoringOrder methods to compare 2d vectors

---
+++ cpp/common/Util.cc
@@ -0,0 +1,22 @@
+
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+namespace util {
+bool isEqualIgnoringOrder(vector<vector<int>>& vec0,
+                          vector<vector<int>>& vec1) {
+  if (vec0.size() != vec1.size()) return false;
+
+  vector<vector<int>> new_vec0 = vec0;
+  vector<vector<int>> new_vec1 = vec1;
+
+  sort(new_vec0.begin(), new_vec0.end());
+  sort(new_vec1.begin(), new_vec1.end());
+
+  return new_vec0 == new_vec1;
+}
+
+}  // namespace util
---
+++ cpp/common/Util.h
@@ -0,0 +1,9 @@
+
+#include <vector>
+
+using namespace std;
+
+namespace util {
+bool isEqualIgnoringOrder(vector<vector<int>> vec0, vector<vector<int>> vec1);
+
+}
